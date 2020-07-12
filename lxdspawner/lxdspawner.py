759e78f42646cbb16da9bd243da0ff7ffa8e7a58
Miguel Marco <mmarco@unizar.es>
2020-07-12 16:26:50 +0000

    use asyncio subprocess so coroutines don't block the hub

--- lxdspawner/lxdspawner.py
+++ lxdspawner/lxdspawner.py
@@ -6,6 +6,7 @@ from tornado import gen
 import pwd
 import grp
 import time
+import asyncio
 
 class LxdSpawner(Spawner):
     """
@@ -16,26 +17,60 @@ class LxdSpawner(Spawner):
     files_to_push = List([], config = True,  help = "Files to push to the container, with the path to push them to")
 
     filesystems_to_mount = List([], config = True, help = "Filesystems to mount, with (name, source, path) format")
-    
 
 
-    @gen.coroutine
-    def start(self):
+    async def run_command(self, lcmd, env=None):
+        cmd = ''
+        for l in lcmd:
+            cmd += l+' '
+        proc = await asyncio.create_subprocess_shell(cmd,
+                                                    stdout=asyncio.subprocess.PIPE,
+                                                    stderr=asyncio.subprocess.PIPE)
+        inbytes=None
+
+        try:
+            out, eout = await proc.communicate()
+        except:
+            self.log.debug("Exception raised when trying to run command: %s" % command)
+            proc.kill()
+            self.log.debug("Running command failed done kill")
+            out, eout = await proc.communicate()
+            out = out.decode.strip()
+            eout = eout.decode.strip()
+            self.log.error("Subprocess returned exitcode %s" % proc.returncode)
+            self.log.error('Stdout:')
+            self.log.error(out)
+            self.log.error('Stderr:')
+            self.log.error(eout)
+            raise RuntimeError('{} exit status {}: {}'.format(cmd, proc.returncode, eout))
+        else:
+            eout = eout.decode().strip()
+            err = proc.returncode
+            if err != 0:
+                self.log.error("Subprocess returned exitcode %s" % err)
+                self.log.error(eout)
+                raise RuntimeError(eout)
+
+        out = out.decode().strip()
+        return out
+
+
+    async def start(self):
         self.log.debug("Running {}".format(["lxc", "init", self.image, self.user.name]))
-        subprocess.run(["lxc", "init", self.image, self.user.name])
+        await self.run_command(["lxc", "init", self.image, self.user.name])
         self.log.debug("Running {}".format(["lxc", "list", "--format", "csv", "-c", "n4"]))
         for f in self.files_to_push:
             self.log.debug("Running {}".format(["lxc", "file", "push", f[0], self.user.name+'/'+f[1]]))
-            subprocess.run(["lxc", "file", "push", f[0], self.user.name+'/'+f[1]])
+            await self.run_command(["lxc", "file", "push", f[0], self.user.name+'/'+f[1]])
         for m in self.filesystems_to_mount:
             self.log.debug("Running {}".format(["lxc", "config", "device", "add", self.user.name, m[0], "disk", "source="+m[1], "path="+m[2]]))
-            subprocess.run(["lxc", "config", "device", "add", self.user.name, m[0], "disk", "source="+m[1], "path="+m[2]])
+            await self.run_command(["lxc", "config", "device", "add", self.user.name, m[0], "disk", "source="+m[1], "path="+m[2]])
         if self.mem_limit:
             self.log.debug("Running {}".format(["lxc", "config", "set", self.user.name, "limits.memory", str(self.mem_limit//1048576)]))
-            subprocess.run(["lxc", "config", "set", self.user.name, "limits.memory", str(self.mem_limit//1048576)+"MB"])
+            await self.run_command(["lxc", "config", "set", self.user.name, "limits.memory", str(self.mem_limit//1048576)+"MB"])
         if self.cpu_limit:
             self.log.debug("Running {}".format(["lxc", "config", "set", self.user.name, "limits.cpu", str(int(self.cpu_limit))]))
-            subprocess.run(["lxc", "config", "set", self.user.name, "limits.cpu", str(int(self.cpu_limit))])
+            await self.run_command(["lxc", "config", "set", self.user.name, "limits.cpu", str(int(self.cpu_limit))])
         self.port = 3080
         uid = pwd.getpwnam(self.user.name)[2]
         gid = grp.getgrnam(self.user.name)[2]
@@ -49,13 +84,13 @@ class LxdSpawner(Spawner):
         cmd.extend(['--debug'])
         #cmd.extend(['"{}"'.format(' '.join(w for w in self.cmd+self.get_args()))])
         self.log.debug("Running {}".format(["lxc", "config", "set", self.user.name, "security.privileged", "true"]))
-        subprocess.run(["lxc", "config", "set", self.user.name, "security.privileged", "true"])
+        await self.run_command(["lxc", "config", "set", self.user.name, "security.privileged", "true"])
         self.log.debug("Running {}".format(["lxc", "start", self.user.name]))
-        subprocess.run(["lxc", "start", self.user.name])
+        await self.run_command(["lxc", "start", self.user.name])
         ip = ''
         while ip == '':
-            res = subprocess.run(["lxc", "list", "--format", "csv", "-c", "n4"], capture_output=True)
-            lines = res.stdout.decode('utf-8').splitlines()
+            res = await self.run_command(["lxc", "list", "--format", "csv", "-c", "n4"])
+            lines = res.splitlines()
             lines = [l.split(',') for l in lines]
             self.log.debug("user.name = {}".format(self.user.name))
             self.log.debug("lines:::::")
@@ -65,29 +100,25 @@ class LxdSpawner(Spawner):
         self.ip = ip
         self.log.debug("ip : {}".format(self.ip))
         self.log.debug("cmd : {}".format(cmd))
-        #yield self.launch_command(cmd) 
-        self.proc = subprocess.Popen(cmd)
-        #time.sleep(20)
-        #await gen.sleep(20)
+        lcmd = ''
+        for l in cmd:
+            lcmd += l+' '
+        self.proc = await asyncio.create_subprocess_shell(lcmd)
+        self.log.debug("created async process with command: {}".format(lcmd))
+        await gen.sleep(5)
+        self.log.debug("waited 5 seconds")
         return (self.ip, self.port)
 
-    @gen.coroutine
-    def launch_command(self, cmd):
-        subprocess.run(cmd)
+
+    async def stop(self):
+        await self.run_command(["lxc", "stop", self.user.name])
+        await self.run_command(["lxc", "delete", self.user.name])
         return
 
-    @gen.coroutine
-    def stop(self):
-        subprocess.run(["lxc", "stop", self.user.name])
-        subprocess.run(["lxc", "delete", self.user.name])
-        return
-
-    @gen.coroutine
-    def poll(self):
+    async def poll(self):
         res = subprocess.run(["lxc", "list", "--format", "csv", "-c", "ns"], capture_output=True)
         lines = res.stdout.decode('utf-8').splitlines()
         lines = [l.split(',') for l in lines]
-        #self.log.debug("Polling, lines = {}".format(lines))
         status = [l[1] for l in lines if l[0] == self.user.name]
         self.log.debug("Polling {}. Container status = {}".format(self.user.name, status))
         if not status:
@@ -98,5 +129,4 @@ class LxdSpawner(Spawner):
             subprocess.run(["lxc", "stop", self.user.name])
             subprocess.run(["lxc", "delete", self.user.name])
             return 1
-            
-    
+
