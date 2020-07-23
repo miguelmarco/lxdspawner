96fade23c7c7aa72870d4be470bbf7204064d64a
Miguel Marco <mmarco@unizar.es>
2020-07-23 13:15:39 +0000

    Add load balancing according to host weights

--- lxdspawner/lxdspawner.py
+++ lxdspawner/lxdspawner.py
@@ -31,6 +31,8 @@ class LxdSpawner(Spawner):
 
     post_hub_commands = List([], config = True, help = "Commands to be run by the hub right after spawning; {USERNAME} will be substituted.")
 
+    host_weights = Dict({}, config = True, help = "Weight of each host for load balancing. Hosts not included here are considered with a weight of 1.0")
+
     async def run_command(self, lcmd, env=None):
         cmd = ''
         for l in lcmd:
@@ -70,7 +72,29 @@ class LxdSpawner(Spawner):
 
     async def start(self):
         self.log.debug("self.user {}".format(self.user.__dict__))
-        await self.run_command(["lxc", "init", self.image, 'lxdspawner-'+self.user.name])
+        existing_hosts_csv = await self.run_command(["lxc", "cluster", "list", "--format", "csv"])
+        existing_hosts_lines = existing_hosts_csv.splitlines()
+        existing_hosts_names = [l.split(',')[0] for l in existing_hosts_lines]
+        self.log.debug("Existing hosts: {}".format(existing_hosts_names))
+        host_loads = {h:0.0001 for h in existing_hosts_names}
+        self.log.debug("host_loads {}".format(host_loads))
+        loads = await self.run_command(["lxc", "list",  "--format", "csv", "-c", "L"])
+        self.log.debug("loads {}".format(loads))
+        hosts_weights = dict([])
+        for l in loads.splitlines():
+            host_loads[l] = host_loads[l]+1.0
+        self.log.debug("host_loads {}".format(host_loads))
+        for l in existing_hosts_names:
+            if not l in self.host_weights:
+                hosts_weights[l] = 1.0
+            else:
+                hosts_weights[l] = float(self.host_weights[l])
+        self.log.debug("hosts_weights {}".format(hosts_weights))
+        hosts_relative_load = {h: hosts_weights[h]/host_loads[h] for h in existing_hosts_names}
+        self.log.debug("Relative load of hosts: {}".format(hosts_relative_load))
+        select_host = max(hosts_relative_load, key = lambda h: hosts_relative_load[h])
+        self.log.debug("Selected {} for having the highest relative availability".format(select_host))
+        await self.run_command(["lxc", "init", self.image, 'lxdspawner-'+self.user.name, "--target", select_host])
         for f in self.files_to_push:
             await self.run_command(["lxc", "file", "push", f[0], 'lxdspawner-'+self.user.name+'/'+f[1]])
         for m in self.filesystems_to_mount:
